c53df062071130f3a545293efc6d511e93bc8486
ksenia_art <ksenksenia02@gmail.com>
2022-09-17 23:14:04 +0300

    Finish vers

--- algAnnealing.py
+++ algAnnealing.py
@@ -1,4 +1,7 @@
 import random as rm
+import math
+
+from functools import reduce
 
 
 class algAnnealing:
@@ -8,22 +11,97 @@ class algAnnealing:
         # в data храним исходные данные
         self.data = initialData
         # в memberType содержится решение и энергия
-        self.memberType = {
+        self.current = {
             "solution": [i for i in range(int(self.data["sizeBoard"]))],
             "energy": 0.0
         }
 
+        self.temp = []
+        self.energy =[]
+
         for i in range(int(self.data["sizeBoard"])):
-            self.tweakSolution()
+            self.tweakSolution(self.current)
 
-    def tweakSolution(self):
-        x = rm.randint(self.data["sizeBoard"])
-        y = rm.randint(self.data["sizeBoard"])
-        while y != x:
-            self.memberType["solution"][x], self.memberType["solution"][y] = self.memberType["solution"][y], self.memberType["solution"][x]
-            y = rm.randint(self.data["sizeBoard"])
+    #изменение решения
+    def tweakSolution(self, member):
+        x = rm.randint(0, self.data["sizeBoard"] - 1)
+        y = rm.randint(0, self.data["sizeBoard"] - 1)
+        while y == x:
+            y = rm.randint(0, self.data["sizeBoard"] - 1)
+        member["solution"][x], member["solution"][y] = member["solution"][y], member["solution"][x]
 
-    def computeEnergy(self):
+    def checkDiag(self, x1, y1, x2, y2):
 
-        dx = [-1, 1, -1, 1]
-        dy = [-1, 1, -1, 1]
+        return int(abs(x1 - x2) == abs(y1 - y2))
+
+    #оценка решения
+    #подсчет кол-ва конфликтов
+    def computeEnergy(self, member):
+
+        conf = 0
+        for x1, y1 in enumerate(member["solution"]):
+            for x2, y2 in enumerate(member["solution"]):
+                if x1 != x2:
+                    conf += self.checkDiag(x1, y1, x2, y2)
+
+        member["energy"] = conf
+
+        #копирование структуры src типа memberType в cструктуру dest
+    def copySolution(self, dest, src):
+        dest["solution"] = src["solution"].copy()
+        dest["energy"] = src["energy"]
+
+    def process(self):
+        timer = 0
+        solution = False
+        self.computeEnergy(self.current)
+
+        best = {
+            "solution": [i for i in range(int(self.data["sizeBoard"]))],
+            "energy": 100
+        }
+
+        working = {
+            "solution": self.current["solution"],
+            "energy": self.current["energy"]
+        }
+
+        while self.data["startTemp"] > self.data["endTemp"]:
+            #Изменены решения случайным образом
+            for step in range(int(self.data["stepRepChange"])):
+                useNew = False
+
+                #изменяем рабочее решение
+                self.tweakSolution(working)
+                #считаем конфликты
+                self.computeEnergy(working)
+
+                if working["energy"] <= self.current["energy"]:
+                    useNew = True
+                else:
+                    test = rm.random()
+                    delta = working["energy"] - self.current["energy"]
+                    calc = (math.e ** (-delta / self.data["startTemp"]))
+                    if calc > test:
+                        useNew = True
+
+                if useNew:
+                    useNew = False
+                    self.copySolution(self.current, working)
+                    if self.current["energy"] < best["energy"]:
+                        self.copySolution(best, self.current)
+                        solution = True
+                    else:
+                        self.copySolution(working,self.current)
+
+            timer += 1
+            self.temp.append(self.data["startTemp"])
+            self.energy.append(best["energy"])
+            #print(timer, self.data["startTemp"], best["energy"])
+            self.data["startTemp"] *= self.data["alpha"]
+
+        if solution:
+            print(best["energy"])
+            return (best["solution"], self.temp, self.energy)
+        return None
+
--- tkWindow.py
+++ tkWindow.py
@@ -1,5 +1,8 @@
-import algAnnealing
+from matplotlib.figure import Figure
+
+from algAnnealing import algAnnealing
 from tkinter import *
+import matplotlib.pyplot as plt
 
 
 def settingWindow(root):
@@ -14,15 +17,16 @@ def settingWindow(root):
 
     button = Button(root, text="Ввод", command=lambda: button_clicked(
         {
-            "startTemp": startTemp.get(),
-            "endTemp": endTemp.get(),
-            "sizeBoard": sizeBoard.get(),
-            "alpga": alpha.get(),
-            "stepRepChange": stepRepChange.get()
+            "root":root,
+            "startTemp": float(startTemp.get()),
+            "endTemp": float(endTemp.get()),
+            "sizeBoard": int(sizeBoard.get()),
+            "alpha": float(alpha.get()),
+            "stepRepChange": int(stepRepChange.get())
         }
     ))
     button.grid(column=1, row=6)
-    drawBoard(root,8,[i for i in range(int(8))])
+
     root.mainloop()
 
 
@@ -35,20 +39,31 @@ def line_input(root, name, coord):
 
 
 def button_clicked(data):
-    algAnnealing.process(data)
+    alg =  algAnnealing(data)
+    solution = alg.process()
+    if solution == None:
+        label = Label(data["root"], text="Решения нет")
+        label.grid(column=2, row=7)
+    else:
+        drawBoard(data["root"],data["sizeBoard"], solution[0])
+        plt.plot(solution[1], solution[2])
+        plt.xlabel("Температура")
+        plt.ylabel("Энергия")
+        plt.show()
 
 def drawBoard(root, sizeBoard, solution):
-    size = 30
+    size = 10
     bg = 'white'
+
     canvas = Canvas(root, width=sizeBoard * size, height=sizeBoard* size, bg=bg)
     canvas.grid(column=2, row=7)
 
-    for i in range(8):
-        for j in range(8):
+    for i in range(sizeBoard):
+        for j in range(sizeBoard):
             x1, y1, x2, y2 = i * size, j * size, i * size + size, j * size + size
             if solution[i] == j:
                 fill = "red"
             else:
                 fill = 'black' if (i + j) % 2 else 'white'
-            canvas.create_rectangle(x1, y1, x2, y2, fill=fill, outline=bg, width=3, tags="Q")
+            canvas.create_rectangle(x1, y1, x2, y2, fill=fill, outline=bg, width=3)
 
